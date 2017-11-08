1132b03734ee4d4c3dbc67358dbb8b3612cd32ab
Vojta Janoušek <32438265+Vojta95@users.noreply.github.com>
2017-11-08 18:03:06 +0100

    Nahrání hotových entit
    entity: příspěvek, přispěvatel, recenze, stav

---
+++ logos_polytechnikos/src/AppBundle/Entity/Prispevatel.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Table()
+ * @ORM\Entity()
+ */
+class Prispevatel
+{
+	/**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+	
+	/**
+     * @ORM\Column(type="string")
+     * @ORM\Email
+     */
+    private $email;
+	
+	/**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+	
+	/**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+	
+	/**
+     * Set email
+     * @param string $email
+     * @return Prispevatel
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+		return $this;
+    }
+	
+}
+
---
+++ logos_polytechnikos/src/AppBundle/Entity/Prispevek.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Table()
+ * @ORM\Entity()
+ */
+class Prispevek
+{
+	/**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+	
+	/**
+     * @ORM\Column(type="string", unique=true)
+     */
+    protected $hash;
+	
+	/**
+     * @ORM\Column(type="string")
+     */
+    private $nazev;
+	
+	/**
+     * @ORM\Column(type="string")
+     */
+    private $text;	
+	
+	/**
+     * @ORM\Column(type="datetime")
+     */
+    private $datumVytvoreni;
+	
+	
+	 /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Stav", inversedBy="prispevky")
+     * @ORM\JoinColumn(name="stav", referencedColumnName="id")
+     */
+    private $stav;
+	
+	/**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Casopis", inversedBy="prispevky")
+	 * @ORM\JoinColumn(name="casopis", referencedColumnName="id")
+     */
+    private $casopis;
+	
+	/**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Prispevatel", inversedBy="prispevky")
+	 * @ORM\JoinColumn(name="prispevatel", referencedColumnName="id")
+     */
+    private $prispevatel;
+	
+	
+	
+	/**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+	
+	/**
+     * Get hash
+     *
+     * @return string
+     */
+    public function getHash()
+    {
+        return $this->hash;
+    }
+	
+	/**
+     * Set hash
+     * @param string $hash
+     * @return Prispevek
+     */
+    public function setHash($hash)
+    {
+        $this->hash = $hash;
+		return $this;
+    }
+	
+	/**
+     * Get nazev
+     *
+     * @return string
+     */
+    public function getNazev()
+    {
+        return $this->nazev;
+    }
+	
+	/**
+     * Set nazev
+     * @param string $nazev
+     * @return Prispevek
+     */
+    public function setNazev($nazev)
+    {
+        $this->nazev = $nazev;
+		return $this;
+    }
+	
+	/**
+     * Get text
+     *
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+	
+	/**
+     * Set text
+     * @param string $text
+     * @return Prispevek
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+		return $this;
+    }
+	
+	/**
+     * Get datumVytvoreni
+     *
+     * @return integer
+     */
+    public function getDatum()
+    {
+        return $this->datumVytvoreni;
+    }
+	
+	/**
+     * Set datumVytvoreni
+     * @param datetime $datumVytvoreni
+     * @return Prispevek
+     */
+    public function setDatum($datumVytvoreni)
+    {
+        $this->datumVytvoreni = $datumVytvoreni;
+		return $this;
+    }
+	
+	
+	/**
+     * Get stav
+     *
+     * @return \AppBundle\Entity\Stav
+     */
+    public function getStav()
+    {
+        return $this->stav;
+    }
+	
+	/**
+     * Set stav
+     * @param \AppBundle\Entity\Stav $stav
+     * @return Prispevek
+     */
+    public function setStav(\AppBundle\Entity\Stav $stav)
+    {
+        $this->stav = $stav;
+        return $this;
+    }
+	
+	/**
+     * Get casopis
+     *
+     * @return \AppBundle\Entity\Casopis
+     */
+    public function getCasopis()
+    {
+        return $this->casopis;
+    }
+	
+	/**
+     * Set casopis
+     * @param \AppBundle\Entity\Casopis $casopis
+     * @return Prispevek
+     */
+    public function setCasopis(\AppBundle\Entity\Casopis $casopis)
+    {
+        $this->casopis = $casopis;
+        return $this;
+    }
+	
+	/**
+     * Get prispevatel
+     *
+     * @return \AppBundle\Entity\Prispevatel
+     */
+    public function getPrispevatel()
+    {
+        return $this->prispevatel;
+    }
+	
+	/**
+     * Set prispevatel
+     * @param \AppBundle\Entity\Prispevatel $prispevatel
+     * @return Prispevek
+     */
+    public function setStav(\AppBundle\Entity\Prispevatel $prispevatel)
+    {
+        $this->prispevatel = $prispevatel;
+        return $this;
+    }
+}
+
